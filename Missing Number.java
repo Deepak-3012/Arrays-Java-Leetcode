a6dfc0b38c977c505f0fce206027e12369654cba
Deepak S <131615897+Deepak-3012@users.noreply.github.com>
2023-11-25 17:32:19 +0530

    Update Missing Number.java

--- Missing Number.java
+++ Missing Number.java
@@ -2,8 +2,8 @@ class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
         int s1 = (n*(n+1))/2;
-        int s2=0;
-        for(int i=0;i<n;i++)
+        int s2=0;                                        // TC  O(N)
+        for(int i=0;i<n;i++)                             // SC  O(1)   
         {
             s2 += nums[i];
         }
