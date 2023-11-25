a1c8726bc1c3a801b0584be0fc69e802d94c7047
Deepak S <131615897+Deepak-3012@users.noreply.github.com>
2023-11-25 16:56:07 +0530

    Add files via upload

---
+++ Missing Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int s1 = (n*(n+1))/2;
+        int s2=0;
+        for(int i=0;i<n;i++)
+        {
+            s2 += nums[i];
+        }
+        int ans = s1-s2;
+        return ans;
+    }
+}
