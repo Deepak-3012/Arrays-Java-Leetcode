027bf8b7b328f78bb9b7742d20cb805ce1b51406
Deepak-3012 <deepak.20021230@gmail.com>
2024-01-13 03:21:43 +0530

    new program added

---
+++ Contains Duplicate.java
@@ -0,0 +1,15 @@
+// TC - O(n)
+// SC - O(n)
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+     Set<Integer> st = new HashSet<>();
+     for(int x : nums){
+         
+         if(st.contains(x))
+         return true;
+     
+         st.add(x);
+     } 
+     return false;
+    }
+}
